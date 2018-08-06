6bb3d6e0cfee4aca363743428fb0c94758481bf7
Erin Penner <epenner0522@gmail.com>
2018-08-06 20:36:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,9 +15,11 @@ let index = 0;
 
 function konami(e) {
   const key = e.key;
+  console.log(key);
 
   if (key === codes[index]) {
     index++;
+    console.log(index);
 
     if (index === codes.length) {
       alert ("Congratulations!");
